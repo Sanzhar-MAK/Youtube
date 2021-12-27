cffb637a02936a6b47ce9e51bb1963e11cc7a6f7
sanzhar <62831632+Sanzhar-MAK@users.noreply.github.com>
2021-12-27 19:45:31 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6
+        },
+        1600: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 4
+        },
+        1100: {
+            slidesPerView: 3
+        },
+        800: {
+            slidesPerView: 2
+        }
+
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.channel-button-next',
@@ -11,7 +30,16 @@ const swiper = new Swiper('.channel-slider', {
 
 const recommendSwiper = new Swiper('.recommend-slider', {
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1600: {
+            slidesPerView: 3
+        },
+        1100: {
+            slidesPerView: 2
+        }
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.recommend-button-next',
@@ -22,7 +50,27 @@ const recommendSwiper = new Swiper('.recommend-slider', {
 
 const recommendChannelSwiper = new Swiper('.recommend-channel-slider', {
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6
+        },
+        1600: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 4
+        },
+        1100: {
+            slidesPerView: 3
+        },
+        800: {
+            slidesPerView: 2
+        }
+
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.recommend-channel-button-prev',
@@ -30,3 +78,16 @@ const recommendChannelSwiper = new Swiper('.recommend-channel-slider', {
     },
 
 });
+
+const searchButton = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+
+searchButton.addEventListener('click', () => {
+    mobileSearch.classList.toggle('is-open');
+})
+
+if (document.documentElement.scrollWidth <= 640) {
+    swiper.destroy();
+    recommendSwiper.destroy();
+    recommendChannelSwiper.destroy();
+}
